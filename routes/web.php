d445bcd25877c8743071576eae3db42b71c25503
marvinoliveiras <marvinoliveiras@yahoo.es>
2021-05-20 19:01:26 +0100

    finalização das páginas estáticas

--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,24 @@ use Illuminate\Support\Facades\Route;
 Route::namespace('Site')->group(function(){
     Route::get('/', [
         HomeController::class, '__invoke'
-    ]);
+    ])->name('site.home');
     Route::get('produtos', [
         CategoryController::class, 'index'
-    ]);
+    ])->name('site.products');
     Route::get('/produtos/{slug}', [
         CategoryController::class, 'show'
-    ]);
+    ])->name('site.products.category');
    Route::get('/blog',[
        BlogController::class, '__invoke'
-   ]);
-   Route::view('sobre', 'site.about.index');
+   ])->name('site.blog');
+   Route::view('sobre',
+        'site.about.index')
+        ->name('site.about');
    Route::get('contato', [
        ContactController::class, 'index'
-   ]);
+   ])->name('site.contact');
    Route::post('contato', [
        ContactController::class,
        'postContact'
-   ]);
+   ])->name('site.post.contact');
 });
